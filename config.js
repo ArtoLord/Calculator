56d33e4d7ebcfce05b07ed7b8ec1d3f591111c8a
ArtoLord <artgoi@mail.ru>
2018-02-24 22:45:54 +0300

    an update

--- config.js
+++ config.js
@@ -133,7 +133,7 @@ var options = {
 	json: true,
     uri: 'https://api.telegram.org/bot'+getTag()+'/setWebhook',
 	body:{
-		url:
+		url:"http://mysterious-island-79267.herokuapp.com"
 	}
 }
 request(options)
